e0c40f58f2420e57701e8ea6e622479a25058638
kang <ksmcsms12@daum.net>
2023-04-01 18:45:25 +0900

    first upload

---
+++ Excel_1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Excel_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(288, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Excle전송";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(270, 99);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(287, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 27);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "rs232전송";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 123);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "GotoExcel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Excel_1/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel; //버전 : 1.9, 파일버전 : 16
+using Application = Microsoft.Office.Interop.Excel.Application;
+using System.IO;
+using System.IO.Ports;
+
+namespace Excel_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] lines = textBox1.Text.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            List<string> chunks = new List<string>();
+            foreach (string line in lines)
+            {
+                string[] lineChunks = SplitIntoChunks(line, 8);
+                chunks.AddRange(lineChunks);
+            }
+            Application excel = new Application();
+
+            Workbook workbook = excel.Workbooks.Add();
+
+            Worksheet worksheet = workbook.Sheets[1];
+
+            int row = 1;
+            int column = 1;
+            foreach (string chunk in chunks)
+            {
+                worksheet.Cells[row, column].Value = string.Format("{0:0000 0000}", int.Parse(chunk));
+                row++;
+            }
+
+            workbook.SaveAs(@"C:\temp\Test.xlsx");
+
+            excel.Quit();
+
+            workbook = null;
+            excel = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            MessageBox.Show("Excel file created!");
+        }
+        private static string[] SplitIntoChunks(string text, int chunkSize)
+        {
+            List<string> chunks = new List<string>();
+            for (int i = 0; i < text.Length; i += chunkSize)
+            {
+                chunks.Add(text.Substring(i, Math.Min(chunkSize, text.Length - i)));
+            }
+            return chunks.ToArray();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            SerialPort serialPort = new SerialPort("COM3", 115200, Parity.None, 8, StopBits.One);
+            serialPort.Open();
+            string[] chunks = textBox1.Text.Split(new[] { "\r\n" }, StringSplitOptions.None);
+            foreach (string chunk in chunks)
+            {
+                serialPort.WriteLine(chunk);
+            }
+            serialPort.Close();
+        }
+    }
+}
+
+
